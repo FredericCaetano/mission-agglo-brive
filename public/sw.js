50d87a306bab4639caa8c297fa6163890066f7ae
FredericCaetano <senso.009@hotmail.fr>
2026-06-28 22:24:16 +0200

    Add service worker for caching resources

---
+++ public/sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'mission-agglo-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
